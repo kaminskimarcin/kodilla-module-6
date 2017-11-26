938c95de03f13202207c5f1c9ee7607bc7539957
Marcin <kaminski283@gmail.com>
2017-11-26 20:17:51 +0100

    kodilla-modul-7.2

--- kodilla-testing/src/main/java/com/kodilla/testing/library/Book.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/Book.java
@@ -15,12 +15,24 @@ public class Book {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getAuthor() {
         return author;
     }
 
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
     public int getPublicationYear() {
         return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
     }
 
     @Override
