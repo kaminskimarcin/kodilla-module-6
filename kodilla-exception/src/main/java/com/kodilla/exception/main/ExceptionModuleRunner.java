7486235608cfe5fbb5a9556650e55cfb9daa7695
Marcin <kaminski283@gmail.com>
2017-12-02 12:07:41 +0100

    kodilla-modul-8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,13 +1,21 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
+import com.kodilla.exception.test.SecondChallenge;
 
 public class ExceptionModuleRunner {
 
     public static void main(String[] args) {
 
         FileReader fileReader = new FileReader();
-        fileReader.readFile();
+
+        try {
+            fileReader.readFile();
+        } catch (FileReaderException e) {
+            System.out.println("Problem while reading a file!");
+        }
 
     }
 }
